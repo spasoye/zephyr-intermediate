ce7e04b0b89cb4d2e0520b625602a1e464f44543
Ivan <ivan@spasoye.com>
2026-08-18 19:42:18 +0200

    Added COOP thread

--- app/src/main.c
+++ app/src/main.c
@@ -9,13 +9,25 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 #define PRIO_LOW 7
 #define PRIO_MED 7
 #define PRIO_HIGH 7
+#define PRIO_COOP -1
+
+void t_coop_fn(void *p1, void *p2, void *p3){
+    LOG_INF("Starting the COOP thread... ");
+    for(int i = 0; i <= 4; i++){
+        k_busy_wait(5000);
+        LOG_INF("[COOP] Step %d, time %d", i, k_uptime_get_32());                     
+    }
+    LOG_INF("[COOP] Yielding the COOP thread");
+    k_yield();
+    LOG_INF("[COOP] Done");
+}
 
 void t_low_fn(void *p1, void *p2, void *p3)
 {
     int step = 0;
     while (true) {
-        LOG_INF("LOW: step %d, time %d", step++, k_uptime_get_32());
-        k_msleep(300);
+        LOG_INF("[LOW] Step %d, time %d", step++, k_uptime_get_32());
+        k_msleep(300);	
         
     }
 }
@@ -24,7 +36,7 @@ void t_med_fn(void *p1, void *p2, void *p3)
 {
     int step = 0;
     while (true) {
-        LOG_INF("MED: step %d, time %d", step++, k_uptime_get_32());
+        LOG_INF("[MED] Step %d, time %d", step++, k_uptime_get_32());
         k_msleep(200);
     }
 }
@@ -33,10 +45,13 @@ void t_high_fn(void *p1, void *p2, void *p3)
 {
     int step = 0;
     while (true) {
-        LOG_INF("HIGH: step %d, time %d", step++, k_uptime_get_32());
+        LOG_INF("[HIGH] Step %d, time %d", step++, k_uptime_get_32());
         k_msleep(100);
     }
 }
+
+K_THREAD_DEFINE(thread_coop, STACK_SIZE, t_coop_fn,
+                NULL, NULL, NULL, PRIO_COOP, 0, 0);
 
 K_THREAD_DEFINE(thread_low, STACK_SIZE, t_low_fn,
                 NULL, NULL, NULL, PRIO_LOW, 0, 0);
