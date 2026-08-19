5976152d6857c79f9789c7348b91d65e884dd0b6
Ivan <ivan@spasoye.com>
2026-08-19 17:22:00 +0200

    Implemented mutex sync

--- lesson_2/src/main.c
+++ lesson_2/src/main.c
@@ -6,18 +6,21 @@ LOG_MODULE_REGISTER(lesson_2, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define INCREMENTS 10000
+#define INCREMENTS 1000000
 #define A_PRIO 5
 #define B_PRIO 5
 
 static volatile uint32_t cnt = 0;
 static struct k_sem finish_sem;
+static struct k_mutex cnt_mtx;
 
 void thread_a_fn(void *p1, void *p2, void *p3){
     char *name = k_thread_name_get(k_current_get());
 
     for (int i = 0; i < INCREMENTS; i++){
+        k_mutex_lock(&cnt_mtx,K_FOREVER);
         cnt++;
+        k_mutex_unlock(&cnt_mtx);
     }
 
     LOG_INF("[%s] done", name);
@@ -29,7 +32,9 @@ void thread_b_fn(void *p1, void *p2, void *p3){
     char *name = k_thread_name_get(k_current_get());
 
     for (int i = 0; i < INCREMENTS; i++){
+        k_mutex_lock(&cnt_mtx,K_FOREVER);
         cnt++;
+        k_mutex_unlock(&cnt_mtx);
     }
 
     LOG_INF("[%s] done", name);
@@ -49,6 +54,7 @@ int main(void)
     uint32_t time = k_uptime_get_32();
 
     k_sem_init(&finish_sem, 0, 2);
+    k_mutex_init(&cnt_mtx);
 
     LOG_INF("Expected final value: %d", INCREMENTS * 2);
 
